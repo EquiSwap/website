91951addd03e1364cc105cac8683b81d1c30d320
Amaar <amaar.khan02@gmail.com>
2022-04-13 15:27:44 +0100

    Create makeoffer page

---
+++ app/pages/makeoffer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <SignInCard>
+        <Button class="center">
+            <p class="button-text">Confirm Offer</p>
+        </Button>
+    </SignInCard>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.center {
+    display: grid;
+    place-items: center;
+}
+</style>
