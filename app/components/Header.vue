f82feb04dfe4435a03338324c9dd62bb016db362
Ross Munn <ross.munn888@gmail.com>
2022-04-13 12:38:44 +0100

    Header Template

---
+++ app/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script lang="ts">
+export default({
+  name: 'Header'
+})
+</script>
+
+<style>
+</style>
--- app/pages/rossindex.vue
+++ app/pages/rossindex.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <SearchField />
+      <Header />
   </div>
 </template>
 
