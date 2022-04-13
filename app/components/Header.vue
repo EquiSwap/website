c940c86317d582f5e7de8103c527f3773d85da1a
Ross Munn <ross.munn888@gmail.com>
2022-04-13 13:39:58 +0100

    Header created

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,4 +1,14 @@
 <template>
+<div class = "header">
+<img src="~/assets/images/equiswap_logo.png">
+<a class="text" href="#">Categories</a>
+<a class="text" href="#">Leaderboard</a>
+<div class = "spacer"/>
+<div class = "searchbar"><SearchField /></div>
+<a class="text" href="#">Login</a>
+<a class="text" href="#">Register</a>
+<div class = "button"><Button /></div>
+</div>
 </template>
 
 <script lang="ts">
@@ -8,4 +18,40 @@ export default({
 </script>
 
 <style>
+img{
+    max-height: 7%;
+    max-width: 10%;
+    padding-left: 20px;
+}
+
+.header{
+display:flex;
+flex-direction:row;
+background: #ffffff;
+text-align: center;
+}
+
+.spacer{
+flex-grow: 1;
+}
+
+.text{
+font-style:normal;
+font-weight: 500;
+font-size: 21px;
+line-height: 20px;
+display: flex;
+align-items: center;
+color: #6D6D6D;
+padding: 20px;
+}
+
+.searchbar{
+align-self: center;
+}
+
+.button{
+align-self: center;
+}
+
 </style>
